1fd5058f01a0e28fb09ef5124df15ff7e1bad0b3
ChaPhilip <webdevphilc@gmail.com>
2022-11-05 18:58:54 -0700

    add/stories with CKEDITOR

--- routes/stories.js
+++ routes/stories.js
@@ -2,16 +2,15 @@ const express = require('express')
 const router = express.Router()
 const {ensureAuth} = require ('../middleware/auth')//we are destructuring to bring both(list of things) to perform a singular action.get both from same location simultaneously.
 
-const Story =require('../models/Story')
+const Story = require('../models/Story')
 
 //@desc Show add page
 //@route GET/stories/add
-//
-router.get('/add', ensureAUTH,(req,res)=>{
-    res.render('stories/add', {
-        layout:'login',
+//server is going to see /stories and send it to the right route of /add
+router.get('/add', ensureAuth, (req,res)=>{
+    res.render('stories/add') 
+       
     }) 
-})
 
 
 module.exports = router
